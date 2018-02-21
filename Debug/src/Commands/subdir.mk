6f1024536bdea99303cccedf8a4165830e0aed5a
ankbhatia19 <ankbhatia19@gmail.com>
2018-02-20 17:39:07 -0800

    getting rid of this commit soon so it doesnt matter

--- Debug/src/Commands/subdir.mk
+++ Debug/src/Commands/subdir.mk
@@ -10,11 +10,13 @@ CPP_SRCS += \
 ../src/Commands/MoveLift.cpp \
 ../src/Commands/MoveLiftToHeight.cpp \
 ../src/Commands/MoveLiftToMinHeight.cpp \
+../src/Commands/MyAutoCommand.cpp \
 ../src/Commands/OppositeSideSwitch.cpp \
 ../src/Commands/ReleaseCrate.cpp \
 ../src/Commands/SameSideSwitch.cpp \
 ../src/Commands/StopCatcher.cpp \
 ../src/Commands/TankDrive.cpp \
+../src/Commands/TestLiftAutonomous.cpp \
 ../src/Commands/Turn.cpp 
 
 OBJS += \
@@ -24,11 +26,13 @@ OBJS += \
 ./src/Commands/MoveLift.o \
 ./src/Commands/MoveLiftToHeight.o \
 ./src/Commands/MoveLiftToMinHeight.o \
+./src/Commands/MyAutoCommand.o \
 ./src/Commands/OppositeSideSwitch.o \
 ./src/Commands/ReleaseCrate.o \
 ./src/Commands/SameSideSwitch.o \
 ./src/Commands/StopCatcher.o \
 ./src/Commands/TankDrive.o \
+./src/Commands/TestLiftAutonomous.o \
 ./src/Commands/Turn.o 
 
 CPP_DEPS += \
@@ -38,11 +42,13 @@ CPP_DEPS += \
 ./src/Commands/MoveLift.d \
 ./src/Commands/MoveLiftToHeight.d \
 ./src/Commands/MoveLiftToMinHeight.d \
+./src/Commands/MyAutoCommand.d \
 ./src/Commands/OppositeSideSwitch.d \
 ./src/Commands/ReleaseCrate.d \
 ./src/Commands/SameSideSwitch.d \
 ./src/Commands/StopCatcher.d \
 ./src/Commands/TankDrive.d \
+./src/Commands/TestLiftAutonomous.d \
 ./src/Commands/Turn.d 
 
 
@@ -50,7 +56,7 @@ CPP_DEPS += \
 src/Commands/%.o: ../src/Commands/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -I"C:\Users\ankbh/wpilib/cpp/current/include" -I"C:\Users\ankbh\Robotics\Team-3341-PowerUp\src" -I"C:\Users\ankbh/wpilib/user/cpp/include" -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -I"C:\Users\ankbh/wpilib/cpp/current/include" -I"C:\Users\ankbh\New folder\Team-3341-PowerUp\src" -I"C:\Users\ankbh/wpilib/user/cpp/include" -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -23,7 +23,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -I"C:\Users\ankbh/wpilib/cpp/current/include" -I"C:\Users\ankbh\Robotics\Team-3341-PowerUp\src" -I"C:\Users\ankbh/wpilib/user/cpp/include" -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -I"C:\Users\ankbh/wpilib/cpp/current/include" -I"C:\Users\ankbh\New folder\Team-3341-PowerUp\src" -I"C:\Users\ankbh/wpilib/user/cpp/include" -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
